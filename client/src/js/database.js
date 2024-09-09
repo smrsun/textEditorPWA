8df04f1bcb634e830e885eed0fdb04085aea853d
smrsun4 <smrsun4@gmail.com>
2024-09-09 13:08:49 -0400

    fixed bad code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,4 @@
 import { openDB } from 'idb';
-const dbName = 'jate';
-const storeName = 'jate';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -15,30 +13,9 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  try {
-    const db = await openDB('jate', 1);
-    const tx = db.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request = store.put({ content }); //increment the ID
-    await request; 
-    console.log('Content added to the database');
-  } catch (error) {
-    console.error('Error adding content to the database:', error);
-  }
-};
+export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  try {
-    const db = await openDB('jate', 1);
-    const tx = db.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const allContent = await store.getAll(); // Get all records from the store
-    console.log('Retrieved content from the database:', allContent);
-    return allContent; // Return the retrieved content
-  } catch (error) {
-    console.error('Error retrieving content from the database:', error);
-  }
-};
+export const getDb = async () => console.error('getDb not implemented');
+
 initdb();
